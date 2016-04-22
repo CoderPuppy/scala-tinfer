51aaa7d3b54aaef45a50c56f71b3914bde6c1301
CoderPuppy <coderpup@gmail.com>
2016-04-22 17:19:01 -0400

    Proper deduping

--- src/main/scala/cpup/typeInference/Graph.scala
+++ src/main/scala/cpup/typeInference/Graph.scala
@@ -8,8 +8,13 @@ import scala.reflect.internal.util.WeakHashSet
 class Graph {
 	protected val _places = mutable.Set.empty[Place]
 	def places: Set[Place] = _places.toSet
-	protected val _typs = new WeakHashSet[Type]
+
+	protected val _typs = mutable.Set.empty[Type]
 	def typs: Set[Type] = _typs.toSet
+
+	protected val _typIds = mutable.Map.empty[Any, Type]
+	def typIds = _typIds.toMap
+
 	protected val _labels = mutable.Set.empty[Label]
 	def labels = _labels.toSet
 
@@ -20,11 +25,13 @@ class Graph {
 	sealed trait Type {
 		val uuid = UUID.randomUUID()
 		_typs += this
+		_typIds.getOrElseUpdate(id, this)
 
 		protected[typeInference] val _places = mutable.Set.empty[Place]
 		def places = _places.toSet
 
 		def merge(other: Type): Type
+		def id: Any
 	}
 	object Type {
 		private var unknownNames = Stream.from(1).map { n =>
@@ -38,13 +45,15 @@ class Graph {
 			val name = unknownNames.head
 			unknownNames = unknownNames.tail
 
-			override def merge(other: Type) = other
+			def merge(other: Type) = other
+			def id = this
 
 			override def toString = s"unknown:$name"
 		}
 
 		class Identifier(val name: String) extends Type {
-			override def merge(other: Type) = other match {
+			def id = name
+			def merge(other: Type) = other match {
 				case other: Identifier if other.name == name => this
 
 				case _ =>
@@ -55,6 +64,10 @@ class Graph {
 		}
 
 		class Construct(val fn: Place, val arg: Place) extends Type {
+			fn._dependents += this
+			arg._dependents += this
+
+			def id = (fn.typ.id, arg.typ.id)
 			def merge(other: Type) = {
 				other match {
 					case other: Construct =>
@@ -75,38 +88,68 @@ class Graph {
 		val uuid = UUID.randomUUID
 	}
 
-	private var unlabeledNames = Stream.from(1).map { n =>
-		Integer.toString(n, 26).toCharArray.map {
-			case c if c >= '0' && c <= '9' => (c + 17).toChar
-			case c if c >= 'a' && c <= 'z' => (c - 32).toChar
-		}.mkString
-	}
+	protected val merging = new WeakHashSet[Place]
 
 	class Place(protected[typeInference] var _typ: Type) {
+		val uuid = UUID.randomUUID
+
+		protected[typeInference] val _dependents = new WeakHashSet[Type]
+		def dependents = _dependents.toSet
+
 		_places += this
 		_typ._places += this
 		def typ = _typ
 
-		def ++(other: Place) = {
+		dedup
+
+		def ++(other: Place): Place = {
+			if(other == this) return this
+			if(merging.contains(this)) {
+				println("recursive merging", this, other)
+				return this
+			}
+			merging += this
+
 			val oldTyp = _typ
-			_typ = other.typ match {
+
+			_typIds.remove(oldTyp.id)
+			_typIds.remove(other._typ.id)
+
+			_typ = other._typ match {
 				case _: Type.Unknown => oldTyp
 				case otyp => oldTyp.merge(otyp)
 			}
 
-			_typ._places ++= other.typ.places
+			_typ._places ++= other._typ.places
 			_typ._places ++= oldTyp.places
+
+			_typs -= oldTyp
+			_typs -= other._typ
+			_typs += _typ
+			_typIds(_typ.id) = _typ
 
 			for(pl <- _typ._places)
 				pl._typ = _typ
 
-//			_typs -= oldTyp
-//			_typs -= other.typ
-//			_typs += _typ
+			dedup
+			for(dep <- _dependents ++ other._dependents; depP <- dep._places.headOption) {
+				depP.dedup
+			}
+
+			merging -= this
 
 			this
 		}
-		val uuid = UUID.randomUUID
+
+		def dedup {
+			for {
+				reged <- _typIds.get(_typ.id)
+				if _typ != reged
+				regedP <- reged._places.headOption
+			} {
+				regedP ++ this
+			}
+		}
 
 		protected val _labels = mutable.Set.empty[Label]
 		def labels = _labels.toSet
@@ -129,17 +172,14 @@ class Graph {
 
 		def apply(arg: Expr) = Expr.Call(this, arg)
 
+		def force(pl: Place) = Expr.Force(this, pl)
+
 		def label(label: String) = Expr.Label(this, label)
 	}
 	object Expr {
 		trait Use {
 			def expr: Expr
 			def typ: Place
-
-			def force(_typ: Place) = {
-				typ ++ _typ
-				this
-			}
 		}
 
 		case class Undefined(pl: Place) extends Expr {
@@ -195,7 +235,21 @@ class Graph {
 				def expr = Label.this
 				val wrapped = Label.this.wrapped.use
 
-				val typ = wrapped.typ.label(label)
+				wrapped.typ.label(label)
+				def typ = wrapped.typ
+			}
+
+			override def use = new Use
+		}
+
+		case class Force(wrapped: Expr, pl: Place) extends Expr {
+			class Use extends Expr.Use {
+				def expr = Force.this
+				val wrapped = Force.this.wrapped.use
+
+				def typ = wrapped.typ
+
+				typ ++ pl
 			}
 
 			override def use = new Use
--- src/main/scala/cpup/typeInference/Main.scala
+++ src/main/scala/cpup/typeInference/Main.scala
@@ -2,54 +2,64 @@ package cpup.typeInference
 
 object Main {
 	def main(args: Array[String]) {
-		println("digraph {\n")
+		print("digraph {\n")
 
 		val g = new Graph
 
 		var step = 1
 		def printGraph {
 			System.gc()
-			println("subgraph cluster_step" + step + " {\n")
-			println("label = \"Step " + step + "\";\n")
-			println(new DOTPrinter(g, s"step$step-").out.mkString)
-			println("}\n")
+			print("subgraph cluster_step" + step + " {\n")
+			print("label = \"Step " + step + "\";\n")
+			print(new DOTPrinter(g, s"step$step-").out.mkString)
+			print("}\n")
 			step += 1
 		}
 
 		val fn = g.typ("Function").label("fn")
 		val io = g.typ("IO").label("io")
+		val monad = g.typ("Monad").label("monad")
 		val str = g.typ("String").label("str")
 		val unit = g.typ("()").label("unit")
 
-		// bind :: m a -> (a -> m b) -> m b
+		def fun(args: g.Place*)(res: g.Place): g.Place = {
+			args.foldRight(res) { (arg, res) =>
+				fn(arg)(res)
+			}
+		}
+
+		// io-monad :: Monad IO
+		val ioM = g.undefined.force(monad(io)).label("ioM")
+
+		// bind :: Monad m -> m a -> (a -> m b) -> m b
 		val bind = g.scope { () =>
 			val m = g.unknown.label("m")
 			val a = g.unknown.label("a")
 			val b = g.unknown.label("b")
 
-			g.undefined.use.force(fn(m(a))(fn(fn(a)(m(b)))(m(b))).label("bind"))
+			g.undefined.force(fun(monad(m), m(a), fun(a)(m(b)))(m(b))).label("bind").use
 		}
 
-		// seq :: m a -> m b -> m b
-		val seq = g.fn { a => g.fn { b => bind(a)(g.fn { _ => b }) } }.label("seq")
+		// seq :: Monad m -> m a -> m b -> m b
+		val seq = g.fn { m => g.fn { a => g.fn { b => bind(m)(a)(g.fn { _ => b }) } } }.label("seq")
 
 		// getLine :: IO String
 		val getLine = g.scope { () =>
-			g.undefined.use.force(io(str))
+			g.undefined.force(io(str)).use
 		}
 
 		// putStrLn :: String -> IO ()
 		val putStrLn = g.scope { () =>
-			g.undefined.use.force(fn(str)(io(unit)))
+			g.undefined.force(fun(str)(io(unit))).use
 		}
 
 		// main = bind getLine putStrLn
-		val main = seq(bind(getLine)(putStrLn))(getLine)
+		val main = seq(ioM)(bind(ioM)(getLine)(putStrLn))(getLine)
 
 		main.use.typ.label("main")
 
 		printGraph
 
-		println("}")
+		print("}\n")
 	}
 }
